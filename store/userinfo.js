453d0030bd4bfc1a17496bd5662b76861781b490
liangyoyo <liangjyoyo@126.com>
2019-07-31 11:23:28 +0800

    hhh

---
+++ store/userinfo.js
@@ -0,0 +1,14 @@
+export const state= {
+   userinfo:{
+       token:''
+       user:{
+           nickname:'小明同学'
+       }
+   } 
+}
+export const mutations= {
+    
+}
+export const actions= {
+    
+}
